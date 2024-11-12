81012df135586e20aa152b0f3d3db507ae0859f4
shira <shirac472@gmail.com>
2024-11-12 10:09:44 +0200

    hayush

---
+++ py/classex.py
@@ -0,0 +1,35 @@
+age = 20
+name = "shira"
+print (age , name)
+
+x = 10
+y = 5
+sum = x+y
+print (sum , x-y , x*y , x/y)
+
+a = 3
+b = 7
+a , b = b , a
+print (a)
+print (b)
+
+length = 5
+width = 4
+area = length * width
+print (area)
+
+greeting = "hello world"
+print (len(greeting)) , print(greeting[0]) , print (greeting[-1])
+
+first_name = "shira"
+last_name = "chen"
+full_name = first_name + " " + last_name
+print(full_name) 
+
+print(f"my name is {name} and i am {age} years old")
+
+quote = "To be or not to be"
+print(quote.upper()) , print(quote.lower())
+
+word = "python"
+print(word[0:3]) , print(word[-3:]) , print(word[::-1])
